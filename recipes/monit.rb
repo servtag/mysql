235886fe7f6ae06c1aa0f2f6f2941871837c1264
Ludwig Bratke <bratke@stroeermobilemedia.com>
2013-12-18 13:29:42 +0100

    added smm_mysql::monit - recipe

---
+++ recipes/monit.rb
@@ -0,0 +1,10 @@
+service 'monit' do
+  supports :status => false, :restart => true, :reload => true
+  action [:enable, :nothing]
+end
+
+template '/etc/monit/conf.d/mysql.monitrc' do
+  source 'mysql.monitrc.erb'
+  mode 0644
+  notifies :restart, 'service[monit]'
+end
